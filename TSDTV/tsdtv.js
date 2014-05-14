5aab0dc33199f1e766bdbbe6f503046c35e68a1c
DBO-Mobile <mrpadraig08@gmail.com>
2014-05-14 12:12:58 -0400

    fixed issues with blank divs blocking elements, by moving them to the fixed footer

--- TSDTV/tsdtv.js
+++ TSDTV/tsdtv.js
@@ -34,26 +34,24 @@ $(window).bind("resize", function(){
 function vlcAspectRatio(){
 	var vlc = $('#vlc');
 	var maxWidth = window.innerWidth * 0.90; // Max width for the image
-    var maxHeight = window.innerHeight * 0.90;    // Max height for the image
+    //var maxHeight = window.innerHeight * 0.90;    // Max height for the image
 	var ratio = 0;  // Used for aspect ratio
     var width = vlc.width();    // Current image width
     var height = vlc.height();  // Current image height
 	// Check if the current width is larger than the max
-	
-      ratio = maxWidth / width;   // get ratio for scaling image
+	if(maxWidth > 800){
+      //maxWidth = 1000;
+      setNewWidth(800,width,height,ratio);
+  }else{
+      setNewWidth(maxWidth,width,height,ratio);
+  }
+}
+
+function setNewWidth(maxWidth, width, height, ratio){
+  ratio = maxWidth / width;   // get ratio for scaling image
       $('#vlc').attr("width", maxWidth); // Set new width
       $('#vlc').attr("height", height * ratio);  // Scale height based on ratio
       $('.tsd-tv-content').css('width', maxWidth);
       height = height * ratio;    // Reset height to match scaled image
       width = width * ratio;    // Reset width to match scaled image
-	
-	// Check if current height is larger than max
-	
-     /* ratio = maxHeight / height; // get ratio for scaling image
-      $('#vlc').attr("height", maxHeight);   // Set new height
-      $('#vlc').attr("width", width * ratio); 
-      $('.tsd-tv-content').css("width", width * ratio);   // Scale width based on ratio
-      width = width * ratio;    // Reset width to match scaled image
-      height = height * ratio;    // Reset height to match scaled image
-    */
 }
